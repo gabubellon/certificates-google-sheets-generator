54c158b7ce3b82d70fcf7a9c18fd751d2c515e43
Gabriel RF <github@gabrf.com>
2021-08-07 19:02:57 -0300

    Quebrando linha

--- gerar_arte.py
+++ gerar_arte.py
@@ -2,7 +2,24 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-def create_image(pessoa,foto, resumo, palestra):
+def split_text(limite, texto):
+    if len(texto) < limite:
+        return texto
+
+    texto_completo = []
+    linha = ''
+    for palavra in texto.split(' '):
+        if len(palavra) + len(linha) < limite-1:
+            linha = f"{linha} {palavra}"
+        else:
+            texto_completo.append(linha)
+            linha = palavra
+
+    texto_completo.append(linha)
+    return '\n'.join(texto_completo)
+
+
+def create_image(pessoa, foto, resumo, palestra):
 
     fonte_nome_pessoa = ImageFont.truetype('Poppins-Bold.ttf', 38)
     fonte_resumo = ImageFont.truetype('Poppins-Medium.ttf', 30)
@@ -13,14 +30,13 @@ def create_image(pessoa,foto, resumo, palestra):
 
     draw = ImageDraw.Draw(arte)
     draw.text((66,216), pessoa, ('#5c1dff'), font = fonte_nome_pessoa)
-    draw.text((66,307), resumo, ('#5c1dff'), font = fonte_resumo, spacing=10)
-    draw.text((174,790), palestra, ('#5c1dff'), font = fonte_nome_palestra)
+    draw.text((66,307), split_text(30, resumo)[:295], ('#5c1dff'), font = fonte_resumo, spacing=10)
+    palestra = split_text(44, " "*17 + palestra)
+    draw.text((66,790), palestra, ('#5c1dff'), font = fonte_nome_palestra)
 
-    file_name = 'certs/{}_{}{}'.format(pessoa, palestra, '.png')
+    file_name = 'certs/{}_{}.png'.format(pessoa, palestra.strip())
 
     arte.paste(foto, (597, 94), mask)
 
     arte.save(file_name)
-    #arte.show()
 
-create_image('Nome', 'Caminho foto', 'Descrição', 'Título')
